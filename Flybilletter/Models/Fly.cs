fee6c4958d4efb7db7fbc30659a26add5b7afdae
John Kasper Svergja <john.k.svergja@gmail.com>
2017-09-15 14:47:40 +0200

    Legger til Fly- og Kunde modell

---
+++ Flybilletter/Models/Fly.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Flybilletter.Models
+{
+    public class Fly
+    {
+        public int ID { get; set; }
+        public string Modell { get; set; }
+        public bool[][] Sete { get; set; } // true er ledig, false er ikke ledig
+    }
+}
---
+++ Flybilletter/Models/Kunde.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Flybilletter.Models
+{
+    public class Kunde
+    {
+        public int ID { get; set; }
+        public string Etternavn { get; set; }
+        public string Fornavn { get; set; }
+        public DateTime Fodselsdag { get; set; }
+        public string Adresse { get; set; }
+        public string Tlf { get; set; }
+    }
+}
