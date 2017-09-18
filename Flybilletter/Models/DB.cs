c383ee06ccca163b6c2fe7e157553f923712acea
Trygve Vang <trygve.n.vang@gmail.com>
2017-09-18 13:27:12 +0200

    Adding Entity Framework, adding controller, adding definition for database

---
+++ Flybilletter/Models/DB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Flybilletter.Models
+{
+    public class DB : DbContext
+    {
+        public DB() : base("name=Flybilletter")
+        {
+            Database.CreateIfNotExists();
+        }
+        
+        //TODO create DBSet
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
