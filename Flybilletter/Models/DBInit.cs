681e0aa387315a33aeeae6a7dc26f1faab4ec1e0
Trygve Vang <trygve.n.vang@gmail.com>
2017-09-20 11:49:41 +0200

    Populerer databasen med testdata

--- Flybilletter/Models/DBInit.cs
+++ Flybilletter/Models/DBInit.cs
@@ -35,9 +35,97 @@ namespace Flybilletter.Models
 
             };
 
+            var BOOOSL = new Rute()
+            {
+                Fra = BOO,
+                Til = OSL,
+                BasePris = 1199
+            };
+            var OSLBOO = new Rute()
+            {
+                Fra = OSL,
+                Til = BOO,
+                BasePris = 1199
+            };
+            var OSLMXP = new Rute()
+            {
+                Fra = OSL,
+                Til = MXP,
+                BasePris = 1499
+            };
+            var MXPOSL = new Rute()
+            {
+                Fra = MXP,
+                Til = OSL,
+                BasePris = 1499
+            };
+
+            var Ola = new Kunde()
+            {
+                Etternavn = "Nordmann",
+                Fornavn = "Ola",
+                Fodselsdag = new DateTime(1970, 1, 18),
+                Adresse = "Osloveien 1",
+                Tlf = "40000000"
+            };
+            var Kari = new Kunde()
+            {
+                Etternavn = "Nordmann",
+                Fornavn = "Kari",
+                Fodselsdag = new DateTime(1970, 4, 20),
+                Adresse = "Osloveien 1",
+                Tlf = "40000001"
+            };
+            var Junior = new Kunde()
+            {
+                Etternavn = "Nordmann",
+                Fornavn = "Ola Junior",
+                Fodselsdag = new DateTime(1997, 12, 1),
+                Adresse = "Osloveien 1",
+                Tlf = "40000002"
+            };
+
+            var Boeing737_1 = new Fly()
+            {
+                Modell = "Boeing 737",
+                Sete = new bool[25, 6]
+            };
+            var Boeing737_2 = new Fly()
+            {
+                Modell = "Boeing 737",
+                Sete = new bool[25, 6]
+            };
+            var Boeing737_3 = new Fly()
+            {
+                Modell = "Boeing 737",
+                Sete = new bool[25, 6]
+            };
+            var AirbusA380_1 = new Fly()
+            {
+                Modell = "Airbus A380",
+                Sete = new bool[50, 9] //50 rader, 9 seter hver rad
+            };
+            var AirbusA380_2 = new Fly()
+            {
+                Modell = "Airbus A380",
+                Sete = new bool[50, 9] //50 rader, 9 seter hver rad
+            };
+
             context.Flyplasser.Add(OSL);
             context.Flyplasser.Add(BOO);
             context.Flyplasser.Add(MXP);
+            context.Ruter.Add(BOOOSL);
+            context.Ruter.Add(OSLBOO);
+            context.Ruter.Add(OSLMXP);
+            context.Ruter.Add(MXPOSL);
+            context.Kunder.Add(Ola);
+            context.Kunder.Add(Kari);
+            context.Kunder.Add(Junior);
+            context.Fly.Add(Boeing737_1);
+            context.Fly.Add(Boeing737_2);
+            context.Fly.Add(Boeing737_3);
+            context.Fly.Add(AirbusA380_1);
+            context.Fly.Add(AirbusA380_2);
             base.Seed(context);
         }
     }
