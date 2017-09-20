b3bc44d2735277315f1eb9808423c805c2abe667
Jesper Nylend <jesper.nylend@gmail.com>
2017-09-20 13:21:36 +0200

    Setter range paa noen av verdiene

--- Flybilletter/Models/SokViewModel.cs
+++ Flybilletter/Models/SokViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +14,8 @@ namespace Flybilletter.Models
         public Flyplass Til { get; set; }
         public DateTime Avreise { get; set; }
         public DateTime Retur { get; set; }
+        [Range (1, 100)]
+        [Display(Name = "Antall Billetter")]
         public int AntallBilletter { get; set; }
 
 
