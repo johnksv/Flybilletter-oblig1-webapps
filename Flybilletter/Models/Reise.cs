2f3eb1346d3bd3a7b935c2b6080746348d16094a
Jesper Nylend <jesper.nylend@gmail.com>
2017-10-06 11:40:52 +0200

    Legger til info om mellomlanding samt rydder litt i Reise.cs

--- Flybilletter/Models/Reise.cs
+++ Flybilletter/Models/Reise.cs
@@ -8,7 +8,7 @@ namespace Flybilletter.Models
     public class Reise
     {
         public List<Flygning> Flygninger { get; set; }
-
+        public int Mellomlanding { get; set; }
         public Flyplass Fra { get; set; }
         public Flyplass Til { get; set; }
         public double Pris { get; set; }
@@ -28,15 +28,18 @@ namespace Flybilletter.Models
 
         public Reise(Flygning flygning1, Flygning flygning2)
         {
-            Flygninger = new List<Flygning>();
-            Flygninger.Add(flygning1);
-            Flygninger.Add(flygning2);
+            Flygninger = new List<Flygning>
+            {
+                flygning1,
+                flygning2
+            };
 
             this.Fra = flygning1.Rute.Fra;
             this.Til = flygning2.Rute.Til;
             this.Pris = flygning1.Rute.BasePris + flygning2.Rute.BasePris;
             this.Avgang = flygning1.AvgangsTid;
             this.Ankomst = flygning2.AnkomstTid;
+            this.Mellomlanding = 1;
         }
 
         
