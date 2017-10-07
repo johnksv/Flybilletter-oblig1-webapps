41eff0df3d664a25a096930358d583892c431910
John Kasper Svergja <john.k.svergja@gmail.com>
2017-10-07 19:32:54 +0200

    Forslag til endring i Reise.

--- Flybilletter/Models/Reise.cs
+++ Flybilletter/Models/Reise.cs
@@ -8,46 +8,62 @@ namespace Flybilletter.Models
     public class Reise
     {
         public List<Flygning> Flygninger { get; set; }
-        public int Mellomlanding { get; set; }
-        public Flyplass Fra { get; set; }
-        public Flyplass Til { get; set; }
-        public double Pris { get; set; }
-        public DateTime Avgang { get; set; }
-        public DateTime Ankomst { get; set; }
-
-        public Reise(Flygning utenMellomLanding)
+        public int Mellomlanding
         {
-            Flygninger = new List<Flygning>();
-            Flygninger.Add(utenMellomLanding);
-            this.Fra = utenMellomLanding.Rute.Fra;
-            this.Til = utenMellomLanding.Rute.Til;
-            this.Pris = utenMellomLanding.Rute.BasePris;
-            this.Avgang = utenMellomLanding.AvgangsTid;
-            this.Ankomst = utenMellomLanding.AnkomstTid;
+            get
+            {
+                return Flygninger.Count - 1;
+            }
+            private set { }
+        }
+        public Flyplass Fra
+        {
+            get
+            {
+                return Flygninger.First().Rute.Fra;
+            }
+            private set { }
+        }
+        public Flyplass Til
+        {
+            get
+            {
+                return Flygninger.Last().Rute.Til;
+            }
+            private set { }
+        }
+        public double Pris
+        {
+            get
+            {
+                return Flygninger.Select(flygning => flygning.Rute.BasePris).Aggregate((pris1, pris2) => pris1 + pris2);
+            }
+            private set { }
+        }
+        public DateTime Avgang
+        {
+            get
+            {
+                return Flygninger.First().AvgangsTid;
+            }
+            private set { }
+        }
+        public DateTime Ankomst
+        {
+            get
+            {
+                return Flygninger.Last().AnkomstTid;
+            }
+            private set { }
         }
 
-        public Reise(Flygning flygning1, Flygning flygning2)
+        public Reise(Flygning flygning1, params Flygning[] flygninger)
         {
             Flygninger = new List<Flygning>
             {
-                flygning1,
-                flygning2
+                flygning1
             };
-
-            this.Fra = flygning1.Rute.Fra;
-            this.Til = flygning2.Rute.Til;
-            this.Pris = flygning1.Rute.BasePris + flygning2.Rute.BasePris;
-            this.Avgang = flygning1.AvgangsTid;
-            this.Ankomst = flygning2.AnkomstTid;
-            this.Mellomlanding = 1;
+            Flygninger.AddRange(flygninger);
         }
-
-        
-
-        
-
-
-
-
     }
 }
